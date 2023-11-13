f7327156e9678c00e68fc9a9b8130a8753c0a73f
789sw <113761212+789sw@users.noreply.github.com>
2023-11-13 12:51:29 +0800

    多加了威盛

---
+++ 威盛yahoo2388.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+url = "https://tw.stock.yahoo.com/quote/2388.TW" # 威盛
+res = requests.get(url).text
+soup = BeautifulSoup(res,"html5lib")
+price_li = soup.find_all('li',{"class":"price-detail-item"})
+title = []
+content = []
+for price in price_li:
+    aaa = price.find_all('span')
+    title.append(aaa[0].text.strip())
+    content.append(aaa[1].text.strip())
+for i in range(len(content)):
+    print(title[i],content[i])
